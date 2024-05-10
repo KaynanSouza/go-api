23a0aa09d7027a25e30b7b02910c9e50eb16b005
KaynanSouza <kaynan.souza@sempreceub.com>
2024-05-10 18:04:40 -0300

    its over

--- controllers/controller.go
+++ controllers/controller.go
@@ -9,7 +9,9 @@ import (
 
 // Controle de requisição
 func GetInfo(context *gin.Context) { // Define uma função chamada "getInfo" que recebe um ponteiro para um objeto de contexto Gin
-	context.JSON(200, models.Alunos)
+	var alunos []models.Aluno
+	database.DB.Find(&alunos) // Buscar todos os alunos dentro do banco de dados
+	context.JSON(200, alunos)
 }
 
 func GetName(context *gin.Context) {
@@ -31,6 +33,74 @@ func PostAluno(context *gin.Context) {
 	}
 
 	database.DB.Create(&aluno)
+	context.JSON(http.StatusOK, aluno)
+
+}
+
+func FindId(context *gin.Context) {
+	var aluno models.Aluno
+	id := context.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		context.JSON(http.StatusNotFound, gin.H{
+			"not found": "error 404",
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, aluno)
+}
+
+func DeleteAluno(context *gin.Context) {
+	var aluno models.Aluno
+	id := context.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if aluno.ID == 0 {
+		context.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Error 404",
+		})
+		return
+	}
+
+	database.DB.Delete(&aluno, id)
+	context.JSON(http.StatusOK, gin.H{
+		"Sucefull": "Aluno deletado",
+	})
+}
+
+func UpdateAluno(context *gin.Context) {
+	var aluno models.Aluno
+	id := context.Params.ByName("id")
+	database.DB.First(&aluno, id)
+
+	if err := context.ShouldBindJSON(&aluno); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"Error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno)
+	context.JSON(http.StatusOK, gin.H{
+		"Sucefull": "Aluno modificado",
+	})
+
+}
+
+func FindCpf(context *gin.Context) {
+	var aluno models.Aluno
+	cpf := context.Param("cpf")
+	database.DB.Where(&models.Aluno{CPF: cpf}, cpf).First(&aluno)
+
+	if aluno.ID == 0 {
+		context.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Error 404",
+		})
+		return
+	}
+
 	context.JSON(http.StatusOK, aluno)
 
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,8 +7,12 @@ import (
 
 func HandleRequests() {
 	r := gin.Default()
-	r.GET("/server", controllers.GetInfo)
-	r.GET("/:name", controllers.GetName)
+	r.GET("/alunos", controllers.GetInfo)
 	r.POST("/alunos", controllers.PostAluno)
+	r.GET("/alunos/:id", controllers.FindId)
+	r.DELETE("/alunos/:id", controllers.DeleteAluno)
+	r.PATCH("/alunos/:id", controllers.UpdateAluno)
+	r.GET("/:name", controllers.GetName)
+	r.GET("/alunos/cpf/:cpf", controllers.FindCpf)
 	r.Run()
 }
